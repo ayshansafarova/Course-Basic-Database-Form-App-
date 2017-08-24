4e25e91880918ca83ef2fe8f1fa407aff3f19350
Ayshan Safarova <aysensafarova@gmail.com>
2017-08-24 16:24:02 +0400

    first commit

---
+++ CourseDatabase/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace CourseDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lessonBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.courseDataSet = new CourseDatabase.CourseDataSet();
+            this.courseDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.lessonTableAdapter = new CourseDatabase.CourseDataSetTableAdapters.LessonTableAdapter();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lessonBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.courseDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.courseDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 122);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(642, 287);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(231, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Database";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Rockwell", 15.75F);
+            this.button1.Location = new System.Drawing.Point(12, 424);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Show All";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Rockwell", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(565, 424);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 40);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Rockwell", 15.75F);
+            this.button3.Location = new System.Drawing.Point(470, 424);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 40);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.comboBox1.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.lessonBindingSource, "Id", true));
+            this.comboBox1.DataSource = this.lessonBindingSource;
+            this.comboBox1.DisplayMember = "name";
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(194, 82);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(142, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.ValueMember = "Id";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lessonBindingSource
+            // 
+            this.lessonBindingSource.DataMember = "Lesson";
+            this.lessonBindingSource.DataSource = this.courseDataSet;
+            // 
+            // courseDataSet
+            // 
+            this.courseDataSet.DataSetName = "CourseDataSet";
+            this.courseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // courseDataSetBindingSource
+            // 
+            this.courseDataSetBindingSource.DataSource = this.courseDataSet;
+            this.courseDataSetBindingSource.Position = 0;
+            // 
+            // lessonTableAdapter
+            // 
+            this.lessonTableAdapter.ClearBeforeFill = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Filter by lesson name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(667, 493);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lessonBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.courseDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.courseDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.BindingSource courseDataSetBindingSource;
+        private CourseDataSet courseDataSet;
+        private System.Windows.Forms.BindingSource lessonBindingSource;
+        private CourseDataSetTableAdapters.LessonTableAdapter lessonTableAdapter;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CourseDatabase/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CourseDatabase
+{
+    public partial class Form1 : Form
+    {
+        CourseEntities db;
+        SqlConnection connection;
+        SqlDataAdapter dataAdapter;
+        SqlCommandBuilder commandBuilder;
+        DataTable dataTable;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hey! Look at this: \n" +
+                "1 - By pressing 'Show' button, you will see the list of students. \n" +
+                "2 - After filtering, editing, adding and deleting data, you should press 'Update' button if you want to save changes in the main database(!!!) \n" +
+                "3 - You don't need 'Add' button in order to add data.\n" +
+                "4 - You can also sort by columns.", "Information",MessageBoxButtons.OK, MessageBoxIcon.Information);
+            addCombo();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(@"data source=(localdb)\MSSQLLocalDB;initial catalog=Course;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework");
+            string command = "SELECT * FROM Student";
+            connection.Open();
+            dataAdapter = new SqlDataAdapter(command, connection);
+            dataTable = new DataTable();
+            //dataSet = new System.Data.DataSet();
+            //dataGridView1.DataSource = dataSet.Tables[0];
+            //dataAdapter.Fill(dataSet, "Student");
+            dataGridView1.DataSource = dataTable;
+            dataAdapter.Fill(dataTable);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            commandBuilder = new SqlCommandBuilder(dataAdapter);
+            //dataAdapter.Update(dataSet, "Student");
+            dataAdapter.Update(dataTable);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you serious to delete this row?!", "Delete", MessageBoxButtons.YesNo) == DialogResult.Yes) ;
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[0].Index);
+                //dataAdapter.Update(dataSet, "Student");
+            }
+        }
+
+        private void addCombo()
+        {
+            db = new CourseEntities();
+            comboBox1.DataSource = db.Lesson.ToList();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Student.Where(x => 
+                            x.lesson_id == comboBox1.SelectedIndex+1).ToList();
+
+        }
+    }
+}
+
